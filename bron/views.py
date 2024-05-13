908b22c2303321667e56e44e40c5a2b1e686a502
AsadbekTohtorov <asadbektohtorov@gmail.com>
2024-05-13 16:29:25 +0600

    Добавил возможность бронирование через форму

--- bron/views.py
+++ bron/views.py
@@ -7,14 +7,31 @@ def index(request):
     context = {'brons': brons}
     if request.user.is_superuser:
         if request.method == 'POST':
-            confirm = request.POST.get('confirm')
-            bron_id = request.POST.get('bron_id')
-            bron = Bron.objects.get(pk=bron_id)
-            if confirm == 'on':
-                bron.admin_confirmed = True
-                bron.save()
-            else:
-                bron.admin_confirmed = False
-                bron.save()
+            if request.POST.get('form1'):
+                confirm = request.POST.get('confirm')
+                bron_id = request.POST.get('bron_id')
+                bron = Bron.objects.get(id=bron_id)
+                if confirm == 'on':
+                    bron.admin_confirmed = True
+                    bron.save()
+                else:
+                    bron.admin_confirmed = False
+                    bron.save()
 
+    if request.method == 'POST':
+        if request.POST.get('form2'):
+            origin = request.POST.get('origin')
+            destination = request.POST.get('destination')
+            date = request.POST.get('date')
+            departure_time = request.POST.get('departure_time')
+            arrival_time = request.POST.get('arrival_time')
+
+            Bron.objects.create(
+                origin=origin,
+                destination=destination,
+                date=date,
+                departure_time=departure_time,
+                arrival_time=arrival_time
+
+            )
     return render(request, 'home.html', context=context)
